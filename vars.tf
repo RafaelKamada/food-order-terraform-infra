d14b75a68de2e0c2ee37f801658496fbd26d1698
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-24 12:30:09 -0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "projectName" {
 }
 
 variable "labRole" {
-  default = "arn:aws:iam::198212171636:role/LabRole"
+  default = "arn:aws:iam::276201098979:role/LabRole"
 }
 
 variable "accessConfig" {
@@ -23,7 +23,7 @@ variable "instanceType" {
 }
 
 variable "principalArn" {
-  default = "arn:aws:iam::198212171636:role/voclabs"
+  default = "arn:aws:iam::276201098979:role/voclabs"
 }
 
 variable "policyArn" {
