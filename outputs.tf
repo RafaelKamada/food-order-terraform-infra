a8f10514bc70be716592fdb5c16fc184c5ad3bea
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-24 11:45:06 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "eks_cluster_id" {
-  value = aws_eks_cluster.eks-cluster.id
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks-cluster.name
 }
 
 output "security_group_id" {
@@ -8,4 +8,8 @@ output "security_group_id" {
 
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks-cluster.endpoint
+}
+
+output "eks_cluster_certificate_authority_0_data" {
+  value = aws_eks_cluster.eks-cluster.certificate_authority[0].data
 }
