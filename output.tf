1040e6c184526cbfd8691bd3fd8b9db42380f55a
RafaelKamada <rafael-kamada2007@hotmail.com>
2025-05-17 23:47:08 -0300

    eks terraform ajustes 3

--- output.tf
+++ output.tf
@@ -2,9 +2,11 @@
 output "security_group_id" {
   description = "ID do Security Group do EKS"
   value       = aws_security_group.sg.id
+  depends_on  = [aws_security_group.sg]
 }
 
 output "vpc_id" {
   description = "ID da VPC"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.main_vpc.id
+  depends_on  = [aws_vpc.main_vpc]
 }
