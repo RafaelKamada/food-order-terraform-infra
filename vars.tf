8d2f9afe7bf5806f08fa26db5cbf23891f711dad
RafaelKamada <rafael-kamada2007@hotmail.com>
2025-05-19 18:18:04 -0300

    infra 04

--- vars.tf
+++ vars.tf
@@ -38,38 +38,3 @@ variable "private_subnet_ids" {
 variable "environment" {
   default = "Production-2"
 }
-
-
-# Variáveis MongoDB
-variable "mongodb_name" {
-  default = "FoodOrder_Cardapio"
-}
-
-variable "mongodb_instance_type" {
-  default = "db.t3.small"
-}
-
-variable "mongodb_allocated_storage" {
-  default = 20
-}
-
-variable "mongodb_backup_retention_period" {
-  default = 7
-}
-
-variable "mongodb_subnet_group_name" {
-  description = "Nome do Subnet Group criado na infraestrutura principal"
-  default = "rds_subnet_group"  # Mantendo o mesmo padrão do RDS
-}
-
-variable "mongodb_admin_password" {
-  description = "Password for MongoDB admin user (only for development)"
-  type        = string
-  sensitive   = true
-  default     = "dev123"  # Senha simples para ambiente de desenvolvimento
-}
-
-variable "eks_cluster_db" {
-  description = "Nome do cluster EKS"
-  default     = "EKS-FOOD-ORDER-DB"
-}
